713fc2442e61b80310ba1ceb58ec2c763883de5b
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-12-05 23:40:20 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,30 +4,26 @@ import Navbar from "../components/Navbar";
 import HeroSection from "../components/HeroSection";
 import AboutSection from "../components/AboutSection";
 import ElevatorAnimation from "../components/ElevatorAnimation";
-
 import WhyUsSection from "../components/WhyUsSection";
 import ServicesSection from "../components/ServicesSection";
 import ProjectsSection from "../components/ProjectsSection";
 import ReferencesSection from "../components/ReferencesSection";
 import ContactSection from "../components/ContactSection";
 import GallerySection from "../components/GallerySection";
-
 import Footer from "../components/Footer";
-// Modallar ayrı bir dosyadaysa açabilirsin:
-// import Modals from "../components/Modals";
 
 export default function Page() {
   return (
     <div className="min-h-screen bg-white text-slate-800 overflow-x-hidden">
-      {/* Üst Menü */}
+
+      {/* TOP MENU */}
       <Navbar />
 
-      {/* İçerik */}
+      {/* MAIN CONTENT */}
       <main>
         <HeroSection />
         <AboutSection />
 
-        {/* Asansör animasyonu ayrı component ise */}
         <section className="py-12">
           <div className="mx-auto max-w-6xl px-6">
             <ElevatorAnimation />
@@ -42,11 +38,8 @@ export default function Page() {
         <GallerySection />
       </main>
 
-      {/* Alt Bilgi */}
+      {/* FOOTER */}
       <Footer />
-
-      {/* Eğer modalları ayrı component olarak böldüysek: */}
-      {/* <Modals /> */}
     </div>
   );
 }
