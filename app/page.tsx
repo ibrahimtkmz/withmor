9a3b8ce5adf5012c5383aecc726c1b92caf03f90
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-12-05 23:57:15 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,12 @@ import Footer from "../components/layout/Footer";
 import HeroSection from "../components/sections/HeroSection";
 import AboutSection from "../components/sections/AboutSection";
 import ElevatorAnimation from "../components/sections/ElevatorAnimation";
-import WhyUsSection from "../components/sections/WhyUsSection";
+import WhyUsSection from "../components/WhyUsSection";
 import ServicesSection from "../components/ServicesSection";
 import ProjectsSection from "../components/ProjectsSection";
 import ReferencesSection from "../components/ReferencesSection";
-import ContactSection from "../components/sections/ContactSection";
-import GallerySection from "../components/sections/GallerySection";
+import ContactSection from "../components/ContactSection";
+import GallerySection from "../components/GallerySection";
 
 export default function Page() {
   return (
