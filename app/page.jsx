b2b578d17f3133da9f92b35a66ce11dd7eb21119
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-12-07 02:22:38 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 import { useEffect, useMemo, useState } from "react";
+import SocialFeedSection from "./components/SocialFeedSection";
+
 // --- İKON TANIMLAMALARI (SVG) ---
 const Icons = {
   MapPin: (props) => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z"/><circle cx="12" cy="10" r="3"/></svg>),
@@ -1970,6 +1972,7 @@ export default function App() {
   </div>
 )}
 
+<SocialFeedSection />
 
 
 
