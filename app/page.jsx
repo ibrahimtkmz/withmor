a5bbe982dc04ee39380d5355d0aa5ceb8f8aa147
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-12-07 02:26:46 +0300

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useMemo, useState } from "react";
-import SocialFeedSection from "./components/SocialFeedSection";
+import SocialFeedSection from "./components/SocialFeedSection.jsx";
 
 // --- İKON TANIMLAMALARI (SVG) ---
 const Icons = {
