23496f45dabbc72d59f507014aaeeee8ec0a513c
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-12-07 17:24:18 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          // X-Frame-Options hiç koymuyoruz (SAMEORIGIN olursa iframe bozulur)
+          // { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+
+          {
+            key: 'Content-Security-Policy',
+            // withmor.com.tr ve www.withmor.com.tr bu siteyi iframe içinde açabilsin
+            value:
+              "frame-ancestors 'self' https://withmor.com.tr https://www.withmor.com.tr;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
