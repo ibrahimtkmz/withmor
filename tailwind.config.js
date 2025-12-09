763c4dd0430f02754374f8982c41fee6eae92dad
ibrahimtkmz <ibrahimtkmz@gmail.com>
2025-12-09 11:01:06 +0300

    Restore default Tailwind blue palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-const brandBlue = {
-  50: "#e8edf6",
-  100: "#d4deef",
-  200: "#a8c0df",
-  300: "#7ca0cf",
-  400: "#5383bf",
-  500: "#3569a8",
-  600: "#264f83",
-  700: "#1c3c64",
-  800: "#132c48",
-  900: "#0d2035",
-};
-
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -19,11 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        blue: brandBlue,
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
